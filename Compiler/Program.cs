d0cf184b8ea9f4569fe41f5ad013d9f4023027f6
Isaac <isaacramirezgarrido1@gmail.com>
2019-04-26 11:39:52 +0200

    IF block
    Starting with if system compilation

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -3,13 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Compiler
 {
     class Program
     {
+        private static StreamReader reader;
+
         static void Main(string[] args)
         {
+            reader = new StreamReader(args[1]);
+
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                analizeLine(line);
+            }
         }
+
+
+        //Analize the code if begins an if, loop or var (variable)
+        public static void analizeLine(string pline)
+        {
+            string[] chunks = pline.Split(':');
+
+            switch (chunks[0].ToUpper())
+            {
+                case "IF":
+                    beginIf(chunks[1]);
+                    break;
+                case "VAR":
+                    break;
+                case "LOOP":
+                    break;
+            }
+        }
+
+        //start IF block
+        public static void beginIf(string condition)
+        {
+
+        }
+
+        //end IF block
+        public static void endIf()
+        {
+
+        }
+
     }
 }
